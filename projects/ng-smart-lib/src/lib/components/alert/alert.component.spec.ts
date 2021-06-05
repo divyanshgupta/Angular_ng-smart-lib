d87ade2a1c3f6fef8996056b8c69f2f6ea1d162e
Divyansh Gupta <divyansh.2392@gmail.com>
2021-06-05 10:00:30 +0530

    Spec Added for Ng-Alert Service

--- projects/ng-smart-lib/src/lib/components/alert/alert.component.spec.ts
+++ projects/ng-smart-lib/src/lib/components/alert/alert.component.spec.ts
@@ -1,20 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AlertComponent } from './alert.component';
+import { NgAlertComponent } from './alert.component';
 
 describe('AlertComponent', () => {
-  let component: AlertComponent;
-  let fixture: ComponentFixture<AlertComponent>;
+  let component: NgAlertComponent;
+  let fixture: ComponentFixture<NgAlertComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
-    })
-    .compileComponents();
+      declarations: [NgAlertComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AlertComponent);
+    fixture = TestBed.createComponent(NgAlertComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
