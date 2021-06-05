06f187e1497ffc600909bde57d070b39526d346d
Divyansh Gupta <divyansh.2392@gmail.com>
2021-06-05 23:55:39 +0530

    (refactor)testing

--- projects/ng-smart-lib-showcase/src/app/app.component.ts
+++ projects/ng-smart-lib-showcase/src/app/app.component.ts
@@ -5,18 +5,10 @@ import { NgAlertService } from '../../../ng-smart-lib/src/lib/components/alert/n
   selector: 'app-root',
   template: `
     <ng-alert></ng-alert>
-    <button class="success" (click)="createSuccessMessage()">
-      Create success alert
-    </button>
-    <button class="danger" (click)="createDangerMessage()">
-      Create danger alert
-    </button>
-    <button class="warning" (click)="createWarningMessage()">
-      Create warning alert
-    </button>
-    <button class="info" (click)="createInfoMessage()">
-      Create info alert
-    </button>
+    <button class="success" (click)="createSuccessMessage()">Create success alert</button>
+    <button class="danger" (click)="createDangerMessage()">Create danger alert</button>
+    <button class="warning" (click)="createWarningMessage()">Create warning alert</button>
+    <button class="info" (click)="createInfoMessage()">Create info alert</button>
   `,
   styleUrls: ['./app.component.css'],
 })
@@ -24,6 +16,7 @@ export class AppComponent {
   constructor(private alertService: NgAlertService) {}
 
   public createSuccessMessage(): void {
+    // cons
     this.alertService.createSuccessAlert('Sample success message');
   }
 
